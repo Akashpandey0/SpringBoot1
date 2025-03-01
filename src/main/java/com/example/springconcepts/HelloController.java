29b4bd1f13da302a17b3e3808d0ae0ce03163ca0
Akash Pandey <akashpande027@gmail.com>
2025-03-01 17:30:47 +0530

    pass name as path variable

--- src/main/java/com/example/springconcepts/HelloController.java
+++ src/main/java/com/example/springconcepts/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController  // Marks this class as a Controller
 @RequestMapping("/api")  // Base URL path for endpoints
@@ -25,6 +26,17 @@ class HelloControllerMark {
     // Query Parameter API: localhost:8080/hello/query?name=Mark
     @GetMapping("/query")
     public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
+
+@RestController  // Marks this class as a REST Controller
+@RequestMapping("/hello")  // Base URL path for endpoints
+class HelloController_PathVariable {
+
+    // Path Variable API: localhost:8080/hello/param/Mark
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz!";
     }
 }
